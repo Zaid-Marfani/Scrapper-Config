9d0185e6b424d139cffb4bbbc7f520d7b61f6785
Zaid Marfani <62537316+Zaid-Marfani@users.noreply.github.com>
2026-01-05 16:26:28 +0530

    version system updated..

---
+++ shipping_lines_default.js
@@ -0,0 +1,47 @@
+module.exports = {
+  version: "1.3.0",
+  lines: [
+    {
+      code: "MAE",
+      display_name: "Maersk",
+      scraper_key: "maersk",
+      url: "https://www.maersk.com/tracking/",
+      active: 1
+    },
+    {
+      code: "MSC",
+      display_name: "MSC",
+      scraper_key: "msc",
+      url: "https://www.msc.com/track-a-shipment",
+      active: 1
+    },
+    {
+      code: "ONE",
+      display_name: "ONE Line",
+      scraper_key: "oneline",
+      url: "https://ecomm.one-line.com/one-ecom/manage-shipment/cargo-tracking?trakNoParam=",
+      active: 1
+    },
+    {
+      code: "EVG",
+      display_name: "Evergreen",
+      scraper_key: "evergreen",
+      url: "https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do",
+      active: 1
+    },
+    {
+      code: "SNK",
+      display_name: "Sinokor",
+      scraper_key: "sinokor",
+      url: "https://ebiz.sinokor.co.kr/BLDetail?blno=",
+      active: 1
+    },
+    {
+      code: "KMT",
+      display_name: "KMTC",
+      scraper_key: "kmtc",
+      url: "https://www.ekmtc.com/index.html#/cargo-tracking",
+      active: 1
+    }
+  ]
+};
